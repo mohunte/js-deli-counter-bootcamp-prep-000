bfa7bd06be2fa9b356174fbb31946a41c20be826
Monique Hunte <monhunte@gmail.com>
2018-12-27 19:52:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ var katzDeli = [];
 
 
 
+var ticketNumber =[]
+function takeANumber(ticketNumber,line){
+  
+  return "Welcome! You're ticket number #1"
+}
 
-function takeANumber(line)
+takeANumber(line) // "Welcome! You're ticket number #1" [1]
+takeANumber(line) // "WElcome! You're ticket number #2" [1,2]
+
+nowServing(line) // "Currently serving 1" [2]
+nowServing(line) // "Currently serving 2" []
+
+takeANumber(line) // "Welcome! You're ticket number #3" [3]
 
